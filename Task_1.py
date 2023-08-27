c6302c6a8afbfd68b7af1d9519fa2b1d017b8b1d
ARSHAD-87 <142817750+ARSHAD-87@users.noreply.github.com>
2023-08-27 19:02:35 +0530

    Add files via upload

---
+++ Task_1.py
@@ -0,0 +1,31 @@
+import numpy
+import sklearn
+import pandas as pd
+from sklearn.model_selection import train_test_split
+iris=pd.read_csv(r'C:\Users\HP\Documents\iNeuBytes\irisflowerdataset.csv')
+print(iris.shape)
+X=iris['sepal_length']
+y=iris['petal_length']
+X_train, X_test, y_train, y_test = train_test_split(
+  X,y , random_state=104,test_size=0.25, shuffle=True)
+ 
+# printing out train and test sets
+ 
+print('X_train : ')
+print(X_train.head())
+print(X_train.shape)
+ 
+print('')
+print('X_test : ')
+print(X_test.head())
+print(X_test.shape)
+ 
+print('')
+print('y_train : ')
+print(y_train.head())
+print(y_train.shape)
+ 
+print('')
+print('y_test : ')
+print(y_test.head())
+print(y_test.shape)
